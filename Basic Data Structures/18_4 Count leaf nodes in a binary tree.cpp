ef5c118e753cdbc8fdaeb18586c9749bf137b1a0
Mahmud394 <unmahmud@gmail.com>
2025-01-13 20:01:37 +0600

    practice

---
+++ Basic Data Structures/18_4 Count leaf nodes in a binary tree.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node{
+    public: 
+    int value;
+    Node* left;
+    Node* right;
+
+    Node(int value)
+    {
+        this->value= value;
+        this->left= NULL;
+        this->right= NULL;
+    }
+
+};
+Node* input_tree()
+{
+    int val;
+    cin>>val;
+    Node* root ;
+    if(val == -1)
+        root=NULL;
+    else 
+        root = new Node(val);
+    queue<Node*>q;
+     if(root!=NULL)
+        q.push(root);
+     while(!q.empty())
+    {
+        // 1.ber kore ana
+        Node* f = q.front();
+        q.pop();
+
+        // 2.oi node niye kaj kora
+        int l,r;
+        cin>>l>>r;
+        Node* myleft, *myright;
+        if(l==-1) 
+            myleft=NULL;
+        else
+             myleft = new Node(l);
+        if(r==-1)
+             myright=NULL;
+        else 
+            myright = new Node(r);
+
+        f->left = myleft;
+        f->right = myright;
+
+        // 3. children push kora
+        if(f->left!=NULL)
+            q.push(f->left);
+        if(f->right!=NULL)
+            q.push(f->right);
+
+    }
+    return root;
+}
+
+
+// use recursion
+int count_Leaf_Node(Node* root)
+{
+    if(root == NULL)
+        return 0;
+    if(root->left==NULL && root->right==NULL)
+        return 1;
+    int l = count_Leaf_Node(root->left);
+    int r = count_Leaf_Node(root->right);
+    return l+r;
+}
+int main() 
+{
+    Node* root =  input_tree();
+  
+  cout<<count_Leaf_Node(root)<<endl;
+
+    return 0;
+}
