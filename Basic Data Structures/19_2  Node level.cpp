dcaac5d54beed68b2d01fdcbd3b3a1df1b222558
Mahmud394 <unmahmud@gmail.com>
2025-01-18 21:55:56 +0600

    practice

--- Basic Data Structures/19_2  Node level.cpp
+++ Basic Data Structures/19_2  Node level.cpp
@@ -73,6 +73,39 @@ vector<int> getLeftView(TreeNode<int> *root)
     return ans;
 
 }
+
+\\ right view of binary tree
+
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int>ans;
+    bool fre[3005]= {false};
+     queue<pair<TreeNode*,int>>q;
+    if(root){
+        q.push({root,1});
+    }
+    while(!q.empty())
+    {
+        pair<TreeNode*,int> parent = q.front();
+        q.pop();
+        TreeNode* node = parent.first;
+        int level = parent.second;
+
+        if (fre[level] == false) {
+          ans.push_back(node->val);
+          fre[level] = true;
+        }
+
+        if(node->right)
+            q.push({node->right, level + 1});
+        if(node->left)
+            q.push({node->left, level + 1});
+        
+    }
+    return ans;
+    }
+};
 */
 
 /*
