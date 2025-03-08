45b49e6ee698f996f45d8546909cde6e14597a7b
Mahmud394 <unmahmud@gmail.com>
2025-03-09 00:46:13 +0600

    practice

--- Introduction to Algorithms/0-1 knapsack/knapsack_using_dp.cpp
+++ Introduction to Algorithms/0-1 knapsack/knapsack_using_dp.cpp
@@ -1,26 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 int val[1005],weight[1005];
+int dp[1005][1005];
 
-int knapsack(int i, int mx_weight)   //O(2^N)
+int knapsack(int i, int mx_weight)  //O(NW)
 {
     if(i<0 || mx_weight <= 0)
         return 0;
+    
+    if(dp[i][mx_weight] != -1)
+        return dp[i][mx_weight];
+
     if(weight[i] <= mx_weight)
     {
-        // 2 options
-        // 1. bag a rakhbo, 2. bag a rakhbo na
         int op1 = knapsack(i-1, mx_weight - weight[i]) + val[i];
         int op2 = knapsack(i-1, mx_weight);
-        return max(op1,op2);
+        dp[i][mx_weight] = max(op1,op2);
+        return dp[i][mx_weight];
     }
     else
     {
-        // 1 option
-        // bag a rakhte parbo na
-        return knapsack(i-1, mx_weight);
+        dp[i][mx_weight] = knapsack(i-1, mx_weight);
+        return dp[i][mx_weight];
     }
-    
 }
 
 int main()
@@ -33,6 +35,10 @@ int main()
     for(int i=0;i<n;i++)
         cin >> weight[i];
     cin >> mx_weight;
+
+    for(int i=0;i<=n;i++)
+        for(int j=0;j<=mx_weight;j++)
+            dp[i][j] = -1;
 
     cout << knapsack(n-1,mx_weight) << endl;
 
